0ac9d789550cd8e4830f0c15e109e680f9305bd3
KirylShtan <gambrinas13kn@gmail.com>
2026-03-25 13:03:14 +0100

    Added property variables for jenkins builds

--- backend/src/test/java/clientapp/natadataservicemanagement/NataDataServiceManagementApplicationTests.java
+++ backend/src/test/java/clientapp/natadataservicemanagement/NataDataServiceManagementApplicationTests.java
@@ -2,7 +2,14 @@ package clientapp.natadataservicemanagement;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
+@TestPropertySource(properties = {"SECURITY_USER_PASSWORD=admin123"})
+@TestPropertySource(properties = {"SECURITY_USER_NAME=admin"})
+@TestPropertySource(properties = {"VAULT_SECRET_TOKEN=dummy-token"})
+@TestPropertySource(properties = {"TELEGRAM_BOT_TOKEN=dummy-token"})
+@TestPropertySource(properties = {"TELEGRAM_WEBHOOK_SECRET=dummy-token"})
+@TestPropertySource(properties = {"JWT_SECRET_KEY=dummy-token"})
 @SpringBootTest
 class NataDataServiceManagementApplicationTests {
 
